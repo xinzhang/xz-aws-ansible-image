109fa96f131fa5b6a646cd9178575904ac52ae40
Xin Zhang <xinzhang@Xins-MacBook-Pro.local>
2019-11-14 20:29:20 +1100

    update nodejs server code

--- create-server.sh
+++ create-server.sh
@@ -6,8 +6,8 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Welcome Node.js');
-}).listen(3001, "127.0.0.1");
+}).listen(3000);
 
-console.log('Server running at http://127.0.0.1:3001/');
+console.log('Server running at http://127.0.0.1:3000/');
 
 EOF
